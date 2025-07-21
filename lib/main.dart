a6f80b00bb37d12f24dc36dfaec74da42b18224f
jyo <bharathiinstitute1@gmail.com>
2025-07-21 18:33:22 +0530

    jyo

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,19 @@
-import 'package:flutter_web/material.dart';
-import 'package:portfolio/ui/home.dart';
-
-import 'package:portfolio/utils/screen/screen_utils.dart';
+import 'package:flutter/material.dart';
+import 'ui/home.dart';
+import 'utils/screen/screen_utils.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      color: Color(0xFF2c2f32),
       theme: ThemeData(
-          brightness: Brightness.light,
-          primaryColorBrightness: Brightness.light,
-          accentColorBrightness: Brightness.light
+        brightness: Brightness.light,
+        primaryColor: const Color(0xFF2c2f32),
       ),
       home: MyAppChild(),
     );
@@ -26,9 +21,8 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppChild extends StatefulWidget {
-
   @override
-  _MyAppChildState createState() => _MyAppChildState();
+  State<MyAppChild> createState() => _MyAppChildState();
 }
 
 class _MyAppChildState extends State<MyAppChild> {
@@ -37,6 +31,14 @@ class _MyAppChildState extends State<MyAppChild> {
     // instantiating ScreenUtil singleton instance, as this will be used throughout
     // the app to get screen size and other stuff
     ScreenUtil.instance = ScreenUtil.getInstance()..init(context);
-    return HomePage();
+    return Scaffold(
+      body: Center(
+        child: Text('Hello, this is a sample text!', style: TextStyle(fontSize: 24)),
+      ),
+    );
   }
 }
+
+dependencies:
+  flutter:
+    sdk: flutter
